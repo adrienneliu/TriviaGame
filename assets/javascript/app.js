89e3a8ed48d07e424d0f5c11d930efbaa1427d0b
Adrienne <iliu.adrienne@gmail.com>
2019-06-02 14:03:49 -0700

    function checks the option clicked with the correct answer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,6 @@
 var i =1;
+var correct = 0;
+var incorrect = 0;
 $(document).ready(function (){
 
     //list of questions
@@ -8,9 +10,9 @@ var questionList = {
     3:  "Who lived at 221B, Baker Street, London?",
     4: "What horoscope sign has a crab? ",
     5: "What’s the real name of Siddartha Gautama?",
-    6: "What’s the smallest type of tree in the world? ",
+    6: "What type of tree stay the smallest in the world? ",
     7: "When did the Second World War end?", 
-    8: "Where was Christopher Columbus born?", 
+    8: "Where was paper invented?", 
     9: "What did the 7 dwarves do for a job?",
     10: "Which planet is nearest the sun? "
 
@@ -18,30 +20,30 @@ var questionList = {
 
     //list of answers
 var answerList = {
-    1: ["Clock", "Meat", "Phone", "Tinkerbell"],  
-    2: ["Oxygen", "Hydrogen", "Carbon", "Helium"] ,
-    3: ["Sherlock"],
+    1: ["Clock","Meat","Phone","Tinkerbell"],  
+    2: ["Oxygen","Hydrogen","Carbon","Helium"] ,
+    3: ["Sherlock","The Doctor","TinTin","Agatha Christie"],
+    4:["Aquarius","Cancer","Pisces","Virgo"],
+    5:["Buddha","Dalai Lama","Rinpoche","Gendun Chopel" ],
+    6:["Bonsai","Dwarf Willow","Hyperion","Banyan"],
+    7:["1945","1942","1939","1918"] ,
+    8:["Egypt","China","India","Japan"],
+    9:["Cave Diving","Gardener","Nothing","Miners"],
+    10:["Mercury","Mars","Venus","Pluto"], 
+
+}
+
+var actualAnswer = {
+    1:["Clock"],
+    2:["Oxygen"],
+    3:["Sherlock"],
     4:["Cancer"],
     5:["Buddha"],
     6:["Bonsai"],
     7:["1945"],
     8:["Genoa"],
     9:["Miners"],
-    10:["Mercury"]
-
-}
-
-var actualAnswer={
-    1:"Clock",
-    2:"Oxygen",
-    3: "Sherlock",
-    4:"Cancer",
-    5:"Buddha",
-    6:"Bonsai",
-    7:"1945",
-    8:"Genoa",
-    9:"Miners",
-    10:"Mercury"
+    10:["Mercury"],
 }
 
 
@@ -60,21 +62,42 @@ function nextQuestion(){
     var j=0;
 
     $("#question").text(questionList[i]);
-    $("#choiceOne").html("<br>" + answerList[i][j])
+    $("#choiceOne").html("<br>" +answerList[i][j])
     j++;
-    $("#choiceTwo").html("<br> " + answerList[i][j])
+    $("#choiceTwo").html("<br>" +answerList[i][j])
     j++
-    $("#choiceThree").html("<br> " + answerList[i][j])
+    $("#choiceThree").html("<br>" +answerList[i][j])
     j++;
-    $("#choiceFour").html("<br> " + answerList[i][j])
-    i++;
+    $("#choiceFour").html("<br>" +answerList[i][j])
+    
 
     
 }
 
 $(".choice").on("click", function() {
-nextQuestion();
+console.log(Object.values(actualAnswer[i])[0]);
+
+console.log($(event.target).text());
+if(String($(event.target).text()) === String(Object.values(actualAnswer[i])[0])){
+    correct++;
+    console.log('correct '+correct);
+
+}else{
+    incorrect++; 
+    console.log('incorrect '+incorrect);
+}
+i++;
+console.log('i   '+i)
+if(i<=10){
+    nextQuestion();
+}
+
 })
+
+
+function result(){
+
+}
 // container for questions, answers, and 3 choices 
 
 // timer 
